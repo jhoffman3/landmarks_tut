419c6b9eced72084bbaf6d40257131bcfeb82975
Jon Hoffman <JonathanHoffman2@eaton.com>
2023-07-25 18:12:49 -0700

    through user input

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -6,12 +6,14 @@
 //
 
 import Foundation
+import Combine
 
-var landmarks: [Landmark] = load("landmarkData.json")
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
